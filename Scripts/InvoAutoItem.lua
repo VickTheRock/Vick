3419fe61dc6e562351298633a8c879aa6e06b11c
VickTheRock <BlecTager@ya.ru>
2015-06-13 20:35:24 +0300

    Update InvoAutoItem.lua

--- Scripts/InvoAutoItem.lua
+++ Scripts/InvoAutoItem.lua
@@ -4,7 +4,9 @@ require("libs.ScriptConfig")
 require("libs.Skillshot")
 
 local config = ScriptConfig.new()
-config:SetParameter("HotKey", "Z", config.TYPE_HOTKEY)
+config:SetParameter("ComboKey", "Z", config.TYPE_HOTKEY)
+config:SetParameter("ComboKey1", "C", config.TYPE_HOTKEY)
+config:SetParameter("ComboKey2", "B", config.TYPE_HOTKEY)
 config:Load()
 
 local play = false local target = nil local castQueue = {} local sleep = {0,0,0}
@@ -30,7 +32,147 @@ function Main(tick)
 		end
 	end
 
-	if IsKeyDown(config.HotKey) and not client.chat then
+	if IsKeyDown(config.ComboKey) and not client.chat then
+		target = targetFind:GetClosestToMouse(200)
+		if tick > sleep[1] then
+			if target and target.alive and target.visible and GetDistance2D(target,me) <= 2000 and not target:DoesHaveModifier("modifier_item_blade_mail_reflect") and not target:DoesHaveModifier("modifier_item_lotus_orb_active") and not target:IsMagicImmune() and target:CanDie() then
+				local distance = GetDistance2D(target,me)
+				local dagon = me:FindDagon()
+				local halberd = me:FindItem("item_heavens_halberd")
+				local abyssal = me:FindItem("item_abyssal_blade")
+				local shiva = me:FindItem("item_shivas_guard")
+				local sheep = me:FindItem("item_sheepstick")
+				local ethereal = me:FindItem("item_ethereal_blade")
+				local veil = me:FindItem("item_veil_of_discord")
+				local atos = me:FindItem("item_rod_of_atos")
+				local mom = me:FindItem("item_mask_of_madness")
+				local satanic = me:FindItem("item_satanic")
+				local sheep = me:FindItem("item_sheepstick")
+				local orchid = me:FindItem("item_orchid")
+				local soulring = me:FindItem("item_soul_ring")
+				local slow = target:DoesHaveModifier("modifier_item_ethereal_blade_slow")
+				if dagon and dagon:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_item_ethereal_blade_slow") then
+					table.insert(castQueue,{1000+math.ceil(dagon:FindCastPoint()*1000),dagon,target})
+				end
+				if halberd and halberd:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{1000+math.ceil(halberd:FindCastPoint()*1000),halberd,target})
+				end
+				if shiva and shiva:CanBeCasted() and distance <= 600  and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{100,shiva})
+				end
+				if abyssal and abyssal:CanBeCasted() and me:CanCast()  and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{math.ceil(abyssal:FindCastPoint()*1000),abyssal,target})
+				end
+				if mom and mom:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{1000+math.ceil(mom:FindCastPoint()*1000),mom})        
+				end
+				if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange then
+					table.insert(castQueue,{100,satanic})
+				end
+				if sheep and sheep:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+				table.insert(castQueue,{math.ceil(sheep:FindCastPoint()*800),sheep,target})
+				end
+				if orchid and orchid:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{math.ceil(orchid:FindCastPoint()*1000),orchid,target})
+				end
+				if sheep and sheep:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{math.ceil(sheep:FindCastPoint()*800),sheep,target})
+				end
+				if ethereal and ethereal:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{math.ceil(ethereal:FindCastPoint()*1000),ethereal,target})
+				end
+				if atos and atos:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{math.ceil(atos:FindCastPoint()*1000),atos,target})
+				end
+				if veil and veil:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{1000+math.ceil(veil:FindCastPoint()*1000),veil,target.position})        
+				end
+				if me.mana < me.maxMana*0.5 and soulring and soulring:CanBeCasted() then
+					table.insert(castQueue,{100,soulring})
+				end
+				if dagon and dagon:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{1000+math.ceil(dagon:FindCastPoint()*1000),dagon,target})
+				end
+				if not slow then
+					me:Attack(target)
+				elseif slow then
+					me:Follow(me)
+				end
+				sleep[1] = tick + 200
+			end
+		end
+	end
+	if IsKeyDown(config.ComboKey1) and not client.chat then
+		target = targetFind:GetClosestToMouse(200)
+		if tick > sleep[1] then
+			if target and target.alive and target.visible and GetDistance2D(target,me) <= 2000 and not target:DoesHaveModifier("modifier_item_blade_mail_reflect") and not target:DoesHaveModifier("modifier_item_lotus_orb_active") and not target:IsMagicImmune() and target:CanDie() then
+				local distance = GetDistance2D(target,me)
+				local dagon = me:FindDagon()
+				local halberd = me:FindItem("item_heavens_halberd")
+				local abyssal = me:FindItem("item_abyssal_blade")
+				local shiva = me:FindItem("item_shivas_guard")
+				local sheep = me:FindItem("item_sheepstick")
+				local ethereal = me:FindItem("item_ethereal_blade")
+				local veil = me:FindItem("item_veil_of_discord")
+				local atos = me:FindItem("item_rod_of_atos")
+				local mom = me:FindItem("item_mask_of_madness")
+				local satanic = me:FindItem("item_satanic")
+				local sheep = me:FindItem("item_sheepstick")
+				local orchid = me:FindItem("item_orchid")
+				local soulring = me:FindItem("item_soul_ring")
+				local slow = target:DoesHaveModifier("modifier_item_ethereal_blade_slow")
+				if dagon and dagon:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_item_ethereal_blade_slow") then
+					table.insert(castQueue,{1000+math.ceil(dagon:FindCastPoint()*1000),dagon,target})
+				end
+				if halberd and halberd:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{1000+math.ceil(halberd:FindCastPoint()*1000),halberd,target})
+				end
+				if shiva and shiva:CanBeCasted() and distance <= 600  and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{100,shiva})
+				end
+				if abyssal and abyssal:CanBeCasted() and me:CanCast()  and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{math.ceil(abyssal:FindCastPoint()*1000),abyssal,target})
+				end
+				if mom and mom:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{1000+math.ceil(mom:FindCastPoint()*1000),mom})        
+				end
+				if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange then
+					table.insert(castQueue,{100,satanic})
+				end
+				if sheep and sheep:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+				table.insert(castQueue,{math.ceil(sheep:FindCastPoint()*800),sheep,target})
+				end
+				if orchid and orchid:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{math.ceil(orchid:FindCastPoint()*1000),orchid,target})
+				end
+				if sheep and sheep:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{math.ceil(sheep:FindCastPoint()*800),sheep,target})
+				end
+				if ethereal and ethereal:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{math.ceil(ethereal:FindCastPoint()*1000),ethereal,target})
+				end
+				if atos and atos:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{math.ceil(atos:FindCastPoint()*1000),atos,target})
+				end
+				if veil and veil:CanBeCasted() and me:CanCast() then
+					table.insert(castQueue,{1000+math.ceil(veil:FindCastPoint()*1000),veil,target.position})        
+				end
+				if me.mana < me.maxMana*0.5 and soulring and soulring:CanBeCasted() then
+					table.insert(castQueue,{100,soulring})
+				end
+				if dagon and dagon:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_invoker_tornado") then
+					table.insert(castQueue,{1000+math.ceil(dagon:FindCastPoint()*1000),dagon,target})
+				end
+				if not slow then
+					me:Attack(target)
+				elseif slow then
+					me:Follow(me)
+				end
+				sleep[1] = tick + 200
+			end
+		end
+	end
+	if IsKeyDown(config.ComboKey2) and not client.chat then
 		target = targetFind:GetClosestToMouse(200)
 		if tick > sleep[1] then
 			if target and target.alive and target.visible and GetDistance2D(target,me) <= 2000 and not target:DoesHaveModifier("modifier_item_blade_mail_reflect") and not target:DoesHaveModifier("modifier_item_lotus_orb_active") and not target:IsMagicImmune() and target:CanDie() then
