05efff0ddd222f5236a205bc631d60a1f416f81e
VickTheRock <BlecTager@ya.ru>
2015-06-19 14:16:36 +0300

    Create 1TuskCombo.lua

---
+++ Scripts/1TuskCombo.lua
@@ -0,0 +1,148 @@
+require("libs.Utils")
+require("libs.TargetFind")
+require("libs.HotkeyConfig2")
+require("libs.Skillshot")
+
+ScriptConfig = ConfigGUI:New(script.name)
+script:RegisterEvent(EVENT_KEY, ScriptConfig.Key, ScriptConfig)
+script:RegisterEvent(EVENT_TICK, ScriptConfig.Refresh, ScriptConfig)
+ScriptConfig:SetName("Tusk")
+ScriptConfig:SetExtention(-.3)
+ScriptConfig:SetVisible(false)
+
+ScriptConfig:AddParam("Hotkey","Key",SGC_TYPE_ONKEYDOWN,false,false,68)
+ScriptConfig:AddParam("Ult","UseUlt",SGC_TYPE_TOGGLE,false,true,nil)
+ScriptConfig:AddParam("Diffusal","diff",SGC_TYPE_TOGGLE,false,true,nil)
+ScriptConfig:AddParam("Cheese","Cheese",SGC_TYPE_TOGGLE,false,true,nil)
+ScriptConfig:AddParam("Blink","UseBlink",SGC_TYPE_TOGGLE,false,true,nil)
+
+local play, target, castQueue, castsleep, sleep = false, nil, {}, 0, 0
+
+function Main(tick)
+    if not PlayingGame() then return end
+	local me = entityList:GetMyHero()
+	local ID = me.classId if ID ~= myhero then return end
+
+	for i=1,#castQueue,1 do
+		local v = castQueue[1]
+		table.remove(castQueue,1)
+		local ability = v[2]
+		if type(ability) == "string" then
+			ability = me:FindItem(ability)
+		end
+		if ability and ((me:SafeCastAbility(ability,v[3],false)) or (v[4] and ability:CanBeCasted())) then
+			if v[4] and ability:CanBeCasted() then
+				me:CastAbility(ability,v[3],false)
+			end
+			castsleep = tick + v[1]
+			return
+		end
+	end
+
+	if ScriptConfig.Hotkey and tick > sleep then
+		target = targetFind:GetClosestToMouse(100)
+		if target and GetDistance2D(target,me) <= 2000 and not target:DoesHaveModifier("modifier_item_lotus_orb_active") and not target:IsMagicImmune() and target:CanDie() then
+			local Q, W, E, R = me:GetAbility(1), me:GetAbility(2), me:GetAbility(3), me:GetAbility(6)
+			local distance = GetDistance2D(target,me)
+			local attackRange = me.attackRange	
+			local halberd = me:FindItem("item_heavens_halberd")
+			local abyssal = me:FindItem("item_abyssal_blade")
+			local ethereal = me:FindItem("item_ethereal_blade")
+			local mom = me:FindItem("item_mask_of_madness")
+			local soulring = me:FindItem("item_soul_ring")
+			local satanic = me:FindItem("item_satanic")
+			local slow = target:DoesHaveModifier("modifier_item_ethereal_blade_slow")
+			local diffusal = me:FindItem("item_diffusal_blade") or me:FindItem("item_diffusal_blade_2")
+			local wand = me:FindItem("item_magic_wand")
+			local stick = me:FindItem("item_magic_stick")
+			local cheese = me:FindItem("item_cheese")
+			local inv = me:DoesHaveModifier("modifier_item_invisibility_edge_windwalk") or me:DoesHaveModifier("modifier_item_silver_edge_windwalk")
+			local attackRange = me.attackRange
+			local blink = me:FindItem("item_blink")
+			if (ScriptConfig.Blink) and GetDistance2D(me,target) and blink and blink:CanBeCasted() and me:CanCast() and distance > attackRange+300 and not blink.abilityPhase and not inv then
+				table.insert(castQueue,{1000+math.ceil(blink:FindCastPoint()*1000),blink,target.position})        
+			end
+			if abyssal and abyssal:CanBeCasted() and me:CanCast() and not inv then
+				table.insert(castQueue,{math.ceil(abyssal:FindCastPoint()*1000),abyssal,target})
+			end
+			if (ScriptConfig.Diffusal) and diffusal and diffusal:CanBeCasted() and me:CanCast() and not inv then
+				table.insert(castQueue,{math.ceil(diffusal:FindCastPoint()*800),diffusal,target})
+			end
+			if Q and Q:CanBeCasted() and me:CanCast() and  me:DoesHaveModifier("modifier_tusk_snowball_movement") and not inv then 
+				table.insert(castQueue,{1000+math.ceil(Q:FindCastPoint()*1000),Q,target.position})
+			end
+			if ethereal and ethereal:CanBeCasted() and me:CanCast() and not inv then
+				table.insert(castQueue,{math.ceil(ethereal:FindCastPoint()*1000),ethereal,target})
+			end
+			if W and W:CanBeCasted() and me:CanCast() and not inv then
+				table.insert(castQueue,{1000+math.ceil(W:FindCastPoint()*1000),W,target})        
+			end
+			if W and W:CanBeCasted() and me:CanCast() and  me:DoesHaveModifier("modifier_tusk_snowball_movement") and not inv then
+				table.insert(castQueue,{1000+math.ceil(W:FindCastPoint()*1000),W})        
+			end
+			if distance <= 450 and  E and E:CanBeCasted() and me:CanCast() and  me:DoesHaveModifier("modifier_tusk_snowball_movement") and not inv then
+				table.insert(castQueue,{1000+math.ceil(E:FindCastPoint()*1000),E})        
+			end
+			if mom and mom:CanBeCasted() and me:CanCast() and not inv then
+				table.insert(castQueue,{1000+math.ceil(mom:FindCastPoint()*1000),mom})        
+			end
+			if me.mana < me.maxMana*0.5 and  soulring and soulring:CanBeCasted() and not inv then
+				table.insert(castQueue,{100,soulring})
+			end
+			if halberd and halberd:CanBeCasted() and me:CanCast() and not inv then
+				table.insert(castQueue,{1000+math.ceil(halberd:FindCastPoint()*1000),halberd,target})
+			end
+			if (ScriptConfig.Ult) and R and R:CanBeCasted() and me:CanCast() then
+				table.insert(castQueue,{1000+math.ceil(R:FindCastPoint()*1000),R,target})
+			end
+			if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange+300  and not inv then
+				table.insert(castQueue,{100,satanic})
+			end
+			if (ScriptConfig.Cheese) and cheese and cheese:CanBeCasted() and me.health/me.maxHealth <= 0.3 and distance <= attackRange+600 and not inv then
+				table.insert(castQueue,{100,cheese})
+			end	
+			
+			if wand and wand:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange+600 and not inv then
+				table.insert(castQueue,{100,wand})
+			end	
+			
+			if stick and stick:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange+600 and not inv then
+				table.insert(castQueue,{100,stick})
+			end	
+			if not slow then
+				me:Attack(target)
+			elseif slow then
+				me:Follow(me)
+			end
+			sleep = tick + 100
+		end
+	end
+end
+
+function Load()
+	if PlayingGame() then
+		local me = entityList:GetMyHero()
+		if not me or me.classId ~= CDOTA_Unit_Hero_Tusk then 
+			script:Disable()
+		else
+			play, target, myhero = true, nil, me.classId
+			ScriptConfig:SetVisible(true)
+			script:RegisterEvent(EVENT_TICK, Main)
+			script:UnregisterEvent(Load)
+		end
+	end
+end
+
+function Close()
+	target, myhero = nil, nil
+	ScriptConfig:SetVisible(false)
+	collectgarbage("collect")
+	if play then
+		script:UnregisterEvent(Main)
+		script:RegisterEvent(EVENT_TICK,Load)
+		play = false
+	end
+end
+
+script:RegisterEvent(EVENT_CLOSE, Close) 
+script:RegisterEvent(EVENT_TICK, Load)
