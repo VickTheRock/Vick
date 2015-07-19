ddb0c99da98c36df9a258e40ffa64bf023273495
VickTheRock <BlecTager@ya.ru>
2015-07-19 09:27:47 +0300

    Update 1HuskarCombo.lua

--- Scripts/1HuskarCombo.lua
+++ Scripts/1HuskarCombo.lua
@@ -15,7 +15,7 @@ ScriptConfig:AddParam("Hotkey","Key",SGC_TYPE_ONKEYDOWN,false,false,68)
 ScriptConfig:AddParam("Ult","Ultimate",SGC_TYPE_TOGGLE,false,true,nil)
 ScriptConfig:AddParam("Diffusal","diff",SGC_TYPE_TOGGLE,false,true,nil)
 ScriptConfig:AddParam("Cheese","Cheese",SGC_TYPE_TOGGLE,false,true,nil)
-
+ScriptConfig:AddParam("ArmletTogle","ArmletTogle",SGC_TYPE_TOGGLE,false,true,nil)
 
 local play, target, castQueue, castsleep, sleep = false, nil, {}, 0, 0
 
