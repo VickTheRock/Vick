374fa0b1ce7bfac2a6fea7440c7db26fb8be056c
VickTheRock <BlecTager@ya.ru>
2015-08-04 19:10:02 +0300

    Update 1RubicCombo.lua

--- Scripts/1RubicCombo.lua
+++ Scripts/1RubicCombo.lua
@@ -16,8 +16,13 @@ ScriptConfig:AddParam("KeyD","UseStealSpell",SGC_TYPE_TOGGLE,false,true,nil)
 ScriptConfig:AddParam("Blink","UseBlink",SGC_TYPE_TOGGLE,false,true,nil)
 ScriptConfig:AddParam("UseR","UseUltR",SGC_TYPE_TOGGLE,false,true,nil)
 ScriptConfig:AddParam("GoHomeLowHP","GoHomeLowHP",SGC_TYPE_TOGGLE,false,true,nil)
+ScriptConfig:AddParam("Soul","Soul Ring",SGC_TYPE_TOGGLE,false,true,nil)
+ScriptConfig:AddParam("Arcan","Arcan",SGC_TYPE_TOGGLE,false,true,nil)
+ScriptConfig:AddParam("dagOn","Dagon",SGC_TYPE_TOGGLE,false,true,nil)
 
 local play, target, castQueue, castsleep, sleep = false, nil, {}, 0, 0
+local me = entityList:GetMyHero()
+
 
 function Main(tick)
     if not PlayingGame() then return end
@@ -39,8 +44,7 @@ function Main(tick)
 			return
 		end
 	end
-	
-	
+
 	if ScriptConfig.Hotkey and tick > sleep then
 		target = targetFind:GetClosestToMouse(100)
 		if target and GetDistance2D(target,me) <= 2000 and not target:DoesHaveModifier("modifier_item_blade_mail_reflect") and not target:DoesHaveModifier("modifier_item_lotus_orb_active") and not target:IsMagicImmune() and target:CanDie() then
@@ -57,69 +61,92 @@ function Main(tick)
 			local arcane = me:FindItem("item_arcane_boots")
 			local blink = me:FindItem("item_blink")
 			local attackRange = me.attackRange	
-			local base = entityList:GetEntities({classId = CDOTA_Unit_Fountain,team = meTeam})[1]
-			if (ScriptConfig.Blink) and GetDistance2D(me,target) and blink and blink:CanBeCasted() and me:CanCast() and distance > attackRange+150 and not blink.abilityPhase and not me:IsInvisible()  then
+			
+		--[[	local bases = entityList:GetEntities(function (ent) return ent.classId==CDOTA_Unit_Fountain, end)[1]	
+			local bases = entityList:GetEntities({classId = CDOTA_Unit_Fountain,team = meTeam})[1]
+					if (ScriptConfig.GoHomeLowHP) and bases and SleepCheck("all") and me.health/me.maxHealth < 0.7 then 
+					entityList:GetMyPlayer():Select(me)
+					entityList:GetMyPlayer():Move(bases)
+						Sleep(client.latency+600,"all")
+			end]]
+			if (ScriptConfig.Blink) and GetDistance2D(me,target) and blink and blink:CanBeCasted() and me:CanCast() and distance > attackRange and not blink.abilityPhase and not me:IsInvisible() then
 				table.insert(castQueue,{1000+math.ceil(blink:FindCastPoint()*1000),blink,target.position})        
 			end
-			if ScriptConfig.dagOn and dagon and dagon:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_item_ethereal_blade_slow") and not me:IsInvisible()  then
+			if ScriptConfig.dagOn and dagon and dagon:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_item_ethereal_blade_slow") and not me:IsInvisible() then
 				table.insert(castQueue,{1000+math.ceil(dagon:FindCastPoint()*1000),dagon,target})
 			end
-			if W and W:CanBeCasted() and me:CanCast()  then
+			if W and W:CanBeCasted() and me:CanCast() then
 				me:SafeCastAbility(W,target)
 			end
-			if shiva and shiva:CanBeCasted() and distance <= 600   then
+			if shiva and shiva:CanBeCasted() and distance <= 600 then
 				table.insert(castQueue,{100,shiva})
 			end
-			if sheep and sheep:CanBeCasted() and me:CanCast()   then
+			if sheep and sheep:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{math.ceil(sheep:FindCastPoint()*800),sheep,target})
 			end
-			if orchid and orchid:CanBeCasted() and me:CanCast()   then
+			if orchid and orchid:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{math.ceil(orchid:FindCastPoint()*1000),orchid,target})
 			end
-			if Q and Q:CanBeCasted() and me:CanCast() and not IsPanick(spell) then
+			if Q and Q:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{1000+math.ceil(Q:FindCastPoint()*1000),Q,target})
 			end
-			if Q and Q:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_rubick_telekinesis") and SleepCheck("all")  then
-				table.insert(castQueue,{1000+math.ceil(Q:FindCastPoint()*1000),Q,base.position})
+			if Q and Q:CanBeCasted() and me:CanCast() and target:DoesHaveModifier("modifier_rubick_telekinesis") and SleepCheck("all") then
+				table.insert(castQueue,{1000+math.ceil(Q:FindCastPoint()*1000),Q,me.position})
 						Sleep(client.latency+700,"all")
 			end
-			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and not IsTargetSpell(spell) and not IsGoHome(spell) and not IsPanick(spell) and not me:IsInvisible()   then
-				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,target.position})
-			end
-			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick2(spell) and not IsGoHome(spell) and not IsTargetSpell(spell) and not me:IsInvisible()   then
-				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,target})
-			end
-			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick(spell) and not IsGoHome(spell) and not IsTargetSpell(spell) and not me:IsInvisible()   then
+			if (ScriptConfig.KeyD) and distance <= 325 and D and D:CanBeCasted() and me:CanCast() and not IsTargetSpell(spell) and not me:IsInvisible() then
 				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D})
 			end
-			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick(spell) and not me:IsInvisible() and not IsTargetSpell(spell)   then
-				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,me})
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and not IsTargetSpell(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,target.position})
 			end
-			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and not IsGoHome(spell) and not IsTargetSpell(spell) and not IsPanick(spell) and not me:IsInvisible()   then
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,target.position})
+			end
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick(spell) and not me:IsInvisible() then
 				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,target})
 			end
-			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsMeSpell(spell) and not IsTargetSpell(spell) and not me:IsInvisible()   then
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D})
+			end
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsPanick(spell) and not me:IsInvisible() then
 				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,me})
 			end
-			if ethereal and ethereal:CanBeCasted() and me:CanCast()   then
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and not IsTargetSpell(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,target})
+			end
+			if (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsMeSpell(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,me})
+			end
+			
+			if (ScriptConfig.GoHomeLowHP and ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsGoHome(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,base.position})
+			end
+			if (ScriptConfig.GoHomeLowHP and ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsGoHome(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D})
+			end
+			if (ScriptConfig.GoHomeLowHP and ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsGoHome(spell) and not me:IsInvisible() then
+				table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,me})
+			end
+			if ethereal and ethereal:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{math.ceil(ethereal:FindCastPoint()*1000),ethereal,target})
 			end
-			if atos and atos:CanBeCasted() and me:CanCast()   then
+			if atos and atos:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{math.ceil(atos:FindCastPoint()*1000),atos,target})
 			end
-			if veil and veil:CanBeCasted() and me:CanCast()   then
+			if veil and veil:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{1000+math.ceil(veil:FindCastPoint()*1000),veil,target.position})        
 			end
-			if me.mana < me.maxMana*0.5  and arcane and arcane:CanBeCasted()   then
+			if me.mana < me.maxMana*0.5 and ScriptConfig.Arcan and arcane and arcane:CanBeCasted() then
 				table.insert(castQueue,{100,arcane})
 			end
-			if me.mana < me.maxMana*0.5  and soulring and soulring:CanBeCasted()   then
+			if me.mana < me.maxMana*0.5 and ScriptConfig.Soul and soulring and soulring:CanBeCasted() then
 				table.insert(castQueue,{100,soulring})
 			end
-			if  dagon and dagon:CanBeCasted() and me:CanCast()   then
+			if ScriptConfig.dagOn and dagon and dagon:CanBeCasted() and me:CanCast() then
 				table.insert(castQueue,{1000+math.ceil(dagon:FindCastPoint()*1000),dagon,target})
 			end
-			if ScriptConfig.UseR and R and R:CanBeCasted() and me:CanCast() and D and not me:IsInvisible()   then
+			if ScriptConfig.UseR and R and R:CanBeCasted() and me:CanCast() and D and not me:IsInvisible()  then
 				table.insert(castQueue,{1000+math.ceil(R:FindCastPoint()*1000),R,target})
 			end
 			sleep = tick + 200
@@ -128,15 +155,12 @@ function Main(tick)
 end
 
 function Mains(tick)
-    if not SleepCheck() then return end
+    if not SleepCheck() and ScriptConfig.GoHomeLowHP then return end
 	local me = entityList:GetMyHero()
-		target = targetFind:GetClosestToMouse(100)	
 	local ID = me.classId if ID ~= myhero then return end
-	local foun = {Vector(-7263,-6747,398)}
-	local foun2 = {Vector(7204,6611,398)}
-	local D = me:GetAbility(5)
-	local base = entityList:GetEntities({classId = CDOTA_Unit_Fountain,team = meTeam})[1]
-	if me.controllable and me.unitState ~= -1031241196 and me.health/me.maxHealth < 0.3 and not me:DoesHaveModifier("modifier_bloodseeker_rupture") and me.alive    then
+	foun = {Vector(-7263,-6747,281)}
+	foun2 = {Vector(7204,6611,262)}
+	if me.controllable and me.unitState ~= -1031241196 and me.health/me.maxHealth < 0.5 and not me:DoesHaveModifier("modifier_bloodseeker_rupture") and me.alive then
 		if me.team == LuaEntity.TEAM_RADIANT then
 		me:Move(foun[1])
 			Sleep(1000)
@@ -145,23 +169,12 @@ function Mains(tick)
 			Sleep(1000)
 		end
 	end
-		if (ScriptConfig.GoHomeLowHP) and (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsGoHome(spell) and not me:IsInvisible()  and me:GetDistance2D(base) > 600 then
-			table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,base.position})
-			Sleep(400)
-		end
-		if (ScriptConfig.GoHomeLowHP) and (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsGoHome(spell) and not me:IsInvisible() then
-			table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D})
-			Sleep(400)
-		end
-		if (ScriptConfig.GoHomeLowHP) and (ScriptConfig.KeyD) and D and D:CanBeCasted() and me:CanCast() and IsGoHome(spell) and not me:IsInvisible()  then
-			table.insert(castQueue,{1000+math.ceil(D:FindCastPoint()*1000),D,me})
-			Sleep(400)
-		end
-	end
+end
+
 
 function IsGoHome(spell)
 	local me = entityList:GetMyHero() 
-	if me.health/me.maxHealth < 0.3 and not me:DoesHaveModifier("modifier_bloodseeker_rupture") then
+	if me.health/me.maxHealth <= 0.3 and not me:IsInvisible() then
 	return me:FindSpell("antimage_blink")
 	or me:FindSpell("chen_teleport")
 	or me:FindSpell("faceless_void_time_walk")
@@ -178,18 +191,11 @@ function IsGoHome(spell)
 	or me:FindSpell("wisp_relocate")
 	or me:FindSpell("magnataur_skewer")
 	end
-end	
-
-function IsPanick2(spell)
-	local me = entityList:GetMyHero() 
-	if me.health/me.maxHealth <= 0.4 and not me:IsInvisible() then
-	return me:FindSpell("tusk_snowball")
-	end
 end
 
 function IsPanick(spell)
 	local me = entityList:GetMyHero() 
-	if me.health/me.maxHealth <= 0.4 and not me:IsInvisible() then
+	if me.health/me.maxHealth <= 0.3 and not me:IsInvisible() then
 	return me:FindSpell("omniknight_repel")
 	or me:FindSpell("abaddon_aphotic_shield")
 	or me:FindSpell("bounty_hunter_windwalk")
@@ -205,6 +211,7 @@ function IsPanick(spell)
 	or me:FindSpell("sandking_sandstorm")
 	or me:FindSpell("shadow_demon_disruption")
 	or me:FindSpell("treant_naturesguise")
+	or me:FindSpell("tusk_snowball")
 	or me:FindSpell("warlock_shadow_word")
 	or me:FindSpell("weaver_shukuchi")
 	or me:FindSpell("witchdoctor_voodoo_restoration")
@@ -227,7 +234,25 @@ function IsMeSpell(spell)
 	or me:FindSpell("omniknight_purification")
 	or me:FindSpell("legion_commander_press")
 end
- 
+
+function AllCast(spell)
+	local me = entityList:GetMyHero()
+	return me:FindSpell("bane_fiends_grip")
+	or me:FindSpell("maiden_freezing_field")
+	or me:FindSpell("elder_titan_echo_stomp")
+	or me:FindSpell("enigma_blackhole")
+	or me:FindSpell("keeper_illuminate")
+	or me:FindSpell("nevermore_requiemofsouls")
+	or me:FindSpell("sandking_epicenter")
+	or me:FindSpell("sandking_sandstorm")
+	or me:FindSpell("shadowshaman_shackle")
+	or me:FindSpell("Warlock_Upheaval")
+	or me:FindSpell("witchdoctor_ward")
+	or me:FindSpell("witchdoctor_deathward")
+	or me:FindSpell("pudge_dismember")
+	or me:FindSpell("windrunner_spell_powershot")
+end
+
 function IsTargetSpell(spell)
 	local me = entityList:GetMyHero()
 	return me:FindSpell("invoker_ghost_walk")
@@ -259,19 +284,6 @@ function IsTargetSpell(spell)
 	or me:FindSpell("oracle_fates_edict")
 	or me:FindSpell("bounty_hunter_wind_walk")
 	or me:FindSpell("lion_spell_mana_drain")
-	or me:FindSpell("maiden_freezing_field")
-	or me:FindSpell("elder_titan_echo_stomp")
-	or me:FindSpell("enigma_blackhole")
-	or me:FindSpell("keeper_illuminate")
-	or me:FindSpell("nevermore_requiemofsouls")
-	or me:FindSpell("sandking_epicenter")
-	or me:FindSpell("sandking_sandstorm")
-	or me:FindSpell("shadowshaman_shackle")
-	or me:FindSpell("warlock_upheaval")
-	or me:FindSpell("witchdoctor_ward")
-	or me:FindSpell("witchdoctor_death_ward")
-	or me:FindSpell("pudge_dismember")
-	or me:FindSpell("bane_fiends_grip")
 end
 
 function Load()
