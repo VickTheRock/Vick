de9f0464ac1093efbb4c5498b0e0a712369e3b10
VickTheRock <BlecTager@ya.ru>
2015-08-31 13:54:55 +0300

    Update BroodLastHit.lua

--- Scripts/BroodLastHit.lua
+++ Scripts/BroodLastHit.lua
@@ -1,10 +1,13 @@
---<<Spider LastHit: Beta version. V.0.3.5 >>
+--<<Spider LastHit: Beta version. V.0.4 >>
 require("libs.Utils")
 require("libs.ScriptConfig")
- 
+require("libs.TargetFind")
+require("libs.Skillshot")
  
 config = ScriptConfig.new()
 config:SetParameter("Spider", "D", config.TYPE_HOTKEY)
+config:SetParameter("Chase", "F", config.TYPE_HOTKEY)
+config:SetParameter("ChaseSpider", "E", config.TYPE_HOTKEY)
 config:SetParameter("DenyWithSpider", true)
 config:SetParameter("LastHitWithSpider", true)
 config:SetParameter("UseQlasthit", true)
@@ -54,7 +57,7 @@ end
  
 
 function Main(tick)
-if Spider and tick > sleep then
+if Spider and not (IsKeyDown(config.Chase) or IsKeyDown(config.ChaseSpider)) and tick > sleep then
     if client.pause or client.shopOpen or not SleepCheck() then return end
 	
 		local me = entityList:GetMyHero()
@@ -97,7 +100,7 @@ if Spider and tick > sleep then
 		--end
 		end
 		end
-		if #Spiderlings > 7 then
+		if #Spiderlings > 6 then
 		for i,v in ipairs(enemies) do
 			if v.health > 0 and v.health/v.maxHealth <= 0.4 then
 				for l,tr in ipairs(Spiderlings) do
@@ -138,6 +141,145 @@ if Spider and tick > sleep then
 	end
 end
 
+function Combo(tick)
+	if not PlayingGame() then return end
+	local me = entityList:GetMyHero()
+	local ID = me.classId if ID ~= myhero then return end
+
+
+	for i=1,#castQueue,1 do
+		local v = castQueue[1]
+		table.remove(castQueue,1)
+		local ability = v[2]
+		if type(ability) == "string" then
+			ability = me:FindItem(ability)
+		end
+		if ability and ((me:SafeCastAbility(ability,v[3],false)) or (v[4] and ability:CanBeCasted())) then
+			if v[4] and ability:CanBeCasted() then
+				me:CastAbility(ability,v[3],false)
+			end
+			castsleep = tick + v[1]
+			return
+		end
+	end
+
+	local attackRange = me.attackRange	
+
+	if IsKeyDown(config.Chase) and not client.chat then	
+		v = targetFind:GetClosestToMouse(500)
+		
+		if v and GetDistance2D(me,v) <= 2000 then
+			if tick > sleep then
+					local Q, W, R = me:GetAbility(1), me:GetAbility(2), me:GetAbility(4)
+					local butterfly = me:FindItem("item_butterfly")
+					local moc = me:FindItem("item_medallion_of_courage") or me:FindItem("item_solar_crest")
+					local mom = me:FindItem("item_mask_of_madness")
+					local satanic = me:FindItem("item_satanic")
+					local atos = me:FindItem("item_rod_of_atos")
+					local shiva = me:FindItem("item_shivas_guard")
+					local abyssal = me:FindItem("item_abyssal_blade")
+					local sheep = me:FindItem("item_sheepstick")
+					local halberd = me:FindItem("item_heavens_halberd")
+					local soulring = me:FindItem("item_soul_ring")
+					local BlackKingBar = me:FindItem("item_black_king_bar") 
+					local inv = me:DoesHaveModifier("modifier_broodmother_spin_web_invisible_applier")
+					local orchid = me:FindItem("item_orchid")
+					local distance = GetDistance2D(me,v)
+					local ControlAttack = entityList:GetEntities({classId=CDOTA_Unit_Broodmother_Spiderling, controllable=true, alive=true})
+					if inv and v.alive and v.visible then
+						if orchid and orchid:CanBeCasted() and me:CanCast() then
+							table.insert(castQueue,{math.ceil(orchid:FindCastPoint()*1000),orchid,v})
+						end
+						if Q and Q:CanBeCasted() and me:CanCast() then
+							table.insert(castQueue,{1000+math.ceil(Q:FindCastPoint()*1000),Q,v})
+						end
+						if shiva and shiva:CanBeCasted() and distance <= 500 and not v:DoesHaveModifier("modifier_item_sheepstick") then
+							table.insert(castQueue,{100,shiva})
+						end
+						if abyssal and abyssal:CanBeCasted() and not v:DoesHaveModifier("modifier_item_sheepstick") and not v:IsStunned() then
+							table.insert(castQueue,{math.ceil(atos:FindCastPoint()*1000),abyssal,v})
+						end
+						if (ScriptConfig.Butterfly) and  butterfly and butterfly:CanBeCasted() and me.health/me.maxHealth >= 0.7 and distance >= attackRange+800 then
+							table.insert(castQueue,{100,butterfly})
+						end
+						if atos and atos:CanBeCasted() and me:CanCast() then
+							table.insert(castQueue,{math.ceil(atos:FindCastPoint()*1000),atos,v})
+						end
+						if sheep and sheep:CanBeCasted() and me:CanCast() then
+							table.insert(castQueue,{math.ceil(sheep:FindCastPoint()*1000),sheep,v})
+						end
+						if halberd and halberd:CanBeCasted() and distance < halberd.castRange and not v:IsStunned() and not v:DoesHaveModifier("modifier_item_sheepstick") then
+							table.insert(castQueue,{1000+math.ceil(halberd:FindCastPoint()*1000),halberd,v})
+						end
+						if moc and moc:CanBeCasted() and distance < moc.castRange then
+							table.insert(castQueue,{1000+math.ceil(moc:FindCastPoint()*1000),moc,v})
+						end
+						if mom and mom:CanBeCasted() and distance <= attackRange+200 then
+							table.insert(castQueue,{100,mom})
+						end
+						if R and R:CanBeCasted() and R.cd > 5 and me.health > 800 then
+							table.insert(castQueue,{100,R})
+						end
+						if #ControlAttack > 0 then
+							for i,z in ipairs(ControlAttack) do
+								if z.controllable  then
+								local distance = GetDistance2D(z,v)
+									if distance <= 900 then
+										z:Attack(v)
+									end
+								end
+							end
+						end
+						if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange+50 then
+							table.insert(castQueue,{100,satanic})
+						end
+						if BlackKingBar and BlackKingBar:CanBeCasted() and not inv then
+							local heroes = entityList:GetEntities(function (v) return v.type==LuaEntity.TYPE_HERO and v.alive and v.visible and v.team~=me.team and me:GetDistance2D(v) <= 1200 end)
+							if #heroes > 3 then
+								table.insert(castQueue,{100,BlackKingBar})
+								return
+							end
+						end
+					end
+						if v and distance <= 1500  then
+							if distance > 350 and SleepCheck("all") then
+								me:Follow(v)
+									Sleep(client.latency+200,"all")
+							end
+							if distance < 400 and SleepCheck("all") then
+								me:Attack(v)
+									Sleep(client.latency+300,"all")
+							end
+						end
+					sleep = tick + 300	
+				end	
+			end
+		end	
+	
+	if IsKeyDown(config.ChaseSpider) and not client.chat then
+		if tick > sleep then
+			v = targetFind:GetClosestToMouse(1000)
+			if v and GetDistance2D(me,v) <= 2000 then
+			local ControlAttack = entityList:GetEntities({classId=CDOTA_Unit_Broodmother_Spiderling, controllable=true, alive=true})
+				if #ControlAttack > 4 then
+					for i,z in ipairs(ControlAttack) do
+						if z.controllable  then
+								local distance = GetDistance2D(z,v)
+							if distance <= 2000 then
+								z:Attack(v)
+							end
+						end
+					end
+				end
+			end
+			sleep = tick + 400
+		end
+	end
+end
+
+
+
+
 
 function Load()
     if PlayingGame() then
@@ -148,6 +290,7 @@ function Load()
 			statusText.visible = true
 			play, target, myhero = true, nil, me.classId
 			script:RegisterEvent(EVENT_TICK, Main)
+			script:RegisterEvent(EVENT_TICK, Combo)
 			script:RegisterEvent(EVENT_KEY, Key)
 			script:UnregisterEvent(Load)
         end
@@ -164,6 +307,7 @@ script:UnregisterEvent(Main)
 script:UnregisterEvent(Key)
 statusText.visible = false
 script:RegisterEvent(EVENT_TICK,Load)
+script:RegisterEvent(EVENT_TICK,Combo)
 play = false
         end
 end
